815761f8980f9518cf6117b9a0e996e62063e857
Muhammad Moiz <63865432+MoizMehmood333@users.noreply.github.com>
2022-12-28 17:00:18 +0500

    Add files via upload

---
+++ ClientAsync/Client.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace ClientAsync
+{
+    enum Commands
+    {
+        String = 0,
+        Image = 1
+    }
+    public class Client
+    {
+        public delegate void OnConnectEventHandler(Client sender, bool Connected);
+        public event OnConnectEventHandler OnConnect;
+
+        public delegate void OnSendEventHandler(Client sender, int sent);
+        public event OnSendEventHandler OnSend;
+
+        public delegate void OnDisconnectEventHandler(Client sender);
+        public event OnDisconnectEventHandler OnDisconnect;
+
+        Socket socket;
+        public bool Connected
+        {
+            get {
+                if (socket != null) {
+                    return socket.Connected;
+                }
+                return false;
+            }
+
+        }
+        public Client() {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        }
+        public void Connect(string ipAddress, int nPort) {
+            if (socket == null) {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            }
+            socket.BeginConnect(ipAddress, nPort, ConnectCallBack, null);
+        }
+
+        void ConnectCallBack(IAsyncResult ar) {
+            try
+            {
+                socket.EndConnect(ar);
+                if (OnConnect != null) {
+                    OnConnect(this, Connected);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        public void Send(byte[] data, int index, int length)
+        {
+            socket.BeginSend(BitConverter.GetBytes(length), 0, 4, SocketFlags.None, sendCallBack, null);
+            socket.BeginSend(data, index, length, SocketFlags.None, sendCallBack, null);
+
+        }
+
+        void sendCallBack(IAsyncResult ar) {
+            try
+            {
+                int sent = socket.EndSend(ar);
+                if (OnSend != null) {
+                    OnSend(this, sent);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Send Error\n", ex.Message);
+                
+            }
+        }
+
+        public void Disconnect() {
+
+            try
+            {
+                if (socket.Connected) {
+                    socket.Close();
+                    socket = null;
+                    if (OnDisconnect != null) {
+                        OnDisconnect(this);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    
+
+    }
+
+}
---
+++ ClientAsync/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using ClientAsync;
+using Microsoft.Win32;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ClientAsync
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Client client;
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnDisconnect.Click += BtnDisconnect_Click;
+            btnSendImage.Click += BtnSendImage_Click;
+            btnConnect.Click += BtnConnect_Click;
+            btnSendText.Click += BtnSendText_Click;
+            client = new Client();
+            client.OnConnect += Client_OnConnect;
+            client.OnSend += Client_OnSend;
+            client.OnDisconnect += Client_OnDisconnect;
+        }
+
+        private void Client_OnDisconnect(Client sender)
+        {
+            MessageBox.Show("Disconnected");
+        }
+
+        private void Client_OnSend(Client sender, int sent)
+        {
+            System.Windows.Application.Current.Dispatcher.Invoke(() =>
+            {
+                txtSentOn.Text = string.Format("Data Sent: {0}", sent);
+            });
+        }
+
+        private void Client_OnConnect(Client sender, bool Connected)
+        {
+            if(Connected)
+                MessageBox.Show("Connection Accepted");
+        }
+
+        private void BtnSendText_Click(object sender, RoutedEventArgs e)
+        {
+            SendText(txtToSend.Text);
+        }
+
+        private void BtnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (!client.Connected) {
+                client.Connect("127.0.0.1", 4000);
+            }
+        }
+
+        private void BtnSendImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                SendImage(fileName);
+            }
+        }
+
+        private void BtnDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            client.Disconnect();
+        }
+
+        void SendText(string Text) {
+            BinaryWriter bw = new BinaryWriter(new MemoryStream());
+            bw.Write((int)Commands.String);
+            bw.Write(Text);
+            bw.Close();
+
+            byte[] data = ((MemoryStream)bw.BaseStream).ToArray();
+            bw.BaseStream.Dispose();
+            client.Send(data, 0, data.Length);
+            data = null;
+        }
+        void SendImage(string path) {
+            BinaryWriter bw = new BinaryWriter(new MemoryStream());
+            bw.Write((int)Commands.Image);
+            bw.Write(path);
+            bw.Close();
+            
+            byte[] data = ((MemoryStream)bw.BaseStream).ToArray();
+            bw.BaseStream.Dispose();
+            client.Send(data, 0, data.Length);
+            data = null;
+        }
+    }
+}
